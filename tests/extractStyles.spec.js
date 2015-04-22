a977a787dea205353575ccba1c18d2262a3b6c96
Pete Hunt <pete@smyte.com>
2015-04-22 01:33:18 -0700

    Fix big bug

--- tests/extractStyles.spec.js
+++ tests/extractStyles.spec.js
@@ -10,7 +10,7 @@ describe('extractStyles', function() {
   it('can extract constant styles', function() {
     var rv = extractStyles(EXAMPLE_SRC);
     expect(rv).toEqual({
-      js: "var React = require('react');\n<div\n  style={{\n    \"left\": 2 * LayoutConstants.x\n  }}\n  className=\"__s_0\">\n  <div className=\"__s_1\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</Block>\n",
+      js: "var React = require('react');\n<div\n  style={{\n    \"left\": 2 * LayoutConstants.x\n  }}\n  className=\"__s_0\">\n  <div className=\"__s_1\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</div>\n",
       css: ".__s_0 {\n  width:100%;\n  height:25px;\n  display:block;\n}\n\n.__s_1 {\n  height:24px;\n  display:inline-block;\n}\n\n"
     });
   });
@@ -18,7 +18,7 @@ describe('extractStyles', function() {
   it('can extract simple expressions', function() {
     var rv = extractStyles(EXAMPLE_SRC, {LayoutConstants: {x: 10}});
     expect(rv).toEqual({
-      js: "var React = require('react');\n<div className=\"__s_0\">\n  <div className=\"__s_1\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</Block>\n",
+      js: "var React = require('react');\n<div className=\"__s_0\">\n  <div className=\"__s_1\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</div>\n",
       css: ".__s_0 {\n  width:100%;\n  height:25px;\n  left:20px;\n  display:block;\n}\n\n.__s_1 {\n  height:24px;\n  display:inline-block;\n}\n\n"
     });
   });
@@ -32,7 +32,7 @@ describe('extractStyles', function() {
       };
     });
     expect(rv).toEqual({
-      js: "var React = require('react');\n<div className=\"example_line2\">\n  <div className=\"example_line3\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</Block>\n",
+      js: "var React = require('react');\n<div className=\"example_line2\">\n  <div className=\"example_line3\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</div>\n",
       css: ".example_line2 {\n  /* example.js:2 */\n  width:100%;\n  height:25px;\n  left:20px;\n  display:block;\n}\n\n.example_line3 {\n  /* example.js:3 */\n  height:24px;\n  display:inline-block;\n}\n\n"
     });
   });
