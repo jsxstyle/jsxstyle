e24017a32b30937fe488559b74bb0332c140a05c
Pete Hunt <pete@smyte.com>
2015-04-22 00:18:29 -0700

    more tests

--- tests/extractStyles.spec.js
+++ tests/extractStyles.spec.js
@@ -22,4 +22,18 @@ describe('extractStyles', function() {
       css: ".__s_0 {\n  width:100%;\n  height:25px;\n  left:20px;\n  display:block;\n}\n\n.__s_1 {\n  height:24px;\n  display:inline-block;\n}\n\n"
     });
   });
+
+  it('can create nice looking css', function() {
+    var rv = extractStyles(EXAMPLE_SRC, {LayoutConstants: {x: 10}}, function(entry) {
+      var node = entry.node;
+      return {
+        className: 'example_line' + node.loc.start.line,
+        comment: 'example.js:' + node.loc.start.line,
+      };
+    });
+    expect(rv).toEqual({
+      js: "<div className=\"example_line1\">\n  <div className=\"example_line2\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</Block>\n",
+      css: ".example_line1 {\n  /* example.js:1 */\n  width:100%;\n  height:25px;\n  left:20px;\n  display:block;\n}\n\n.example_line2 {\n  /* example.js:2 */\n  height:24px;\n  display:inline-block;\n}\n\n"
+    });
+  });
 });
