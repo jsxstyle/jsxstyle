431fa1972d919d221fb3e708a89823280770f561
Pete Hunt <pete@smyte.com>
2015-04-22 00:03:02 -0700

    extractStyles starting to work again

---
+++ tests/example.js
@@ -0,0 +1,5 @@
+<Block width="100%" height={25} left={2 * LayoutConstants.x}>
+  <InlineBlock height={24} />
+  <div style={{width: 10}} />
+  <OtherComponent height={25} />
+</Block>
--- tests/extractStyles.spec.js
+++ tests/extractStyles.spec.js
@@ -1,9 +1,21 @@
 'use strict';
 
 var extractStyles = require('../lib/extractStyles');
+var fs = require('fs');
+var path = require('path');
+
+var EXAMPLE_SRC = fs.readFileSync(path.join(__dirname, 'example.js'), {encoding: 'utf8'});
 
 describe('extractStyles', function() {
-  it('works', function() {
+  it('can extract constant styles', function() {
+    var rv = extractStyles(EXAMPLE_SRC);
+    expect(rv).toEqual({
+      js: "<div\n  style={{\n    \"left\": 2 * LayoutConstants.x\n  }}\n  className=\"__s_0\">\n  <div className=\"__s_1\" />\n  <div style={{width: 10}} />\n  <OtherComponent height={25} />\n</Block>\n",
+      css: ".__s_0 {\n  width:100%;\n  height:25px;\n  display:block;\n}\n\n.__s_1 {\n  height:24px;\n  display:inline-block;\n}\n\n"
+    });
+  });
 
+  it('can extract simple expressions', function() {
+    //
   });
 });
