ef36108a87fbe45041ad0df5a5d6aee58ab15e30
Pete Hunt <pete@smyte.com>
2015-05-28 19:39:52 -0700

    Add shading, version bump

--- tests/Color.spec.js
+++ tests/Color.spec.js
@@ -1,8 +1,12 @@
 var Color = require('../lib/Color');
 describe('Color', function() {
-  it('works', function() {
+  it('supports linear gradients', function() {
     expect(Color.linearGradient('to right', [[Color.rgb(255, 255, 255), '5%'], [Color.rgb(255, 0, 0), '25%']])).toBe(
       'linear-gradient(to right, rgb(255, 255, 255) 5%, rgb(255, 0, 0) 25%)'
     );
+  });
+
+  it('supports shades', function() {
+    expect(Color.shade(Color.rgb(255,255,255), 0.929).toString()).toBe('rgb(237, 237, 237)');
   });
 });
