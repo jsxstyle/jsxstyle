ec18fca37cd8747925222d86d1c24164a0fe1326
Pete Hunt <pete@smyte.com>
2016-09-27 15:47:51 -0400

    Expose injectAutoprefixer()

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ var Display = require('./lib/Display');
 var GlobalStylesheets = require('./lib/GlobalStylesheets');
 
 var assign = require('object-assign');
+var createCSS = require('./lib/createCSS');
 var curry = require('./curry');
+var invariant = require('invariant');
 
 var index = assign({
   curry: curry,
   install: GlobalStylesheets.install,
+  injectAutoprefixer(autoprefix) {
+    invariant(typeof autoprefix === 'function', 'You may only inject functions for autoprefix');
+    createCSS.injection.autoprefix = autoprefix;
+  },
 }, Color, Display, Addons);
 
 module.exports = index;
