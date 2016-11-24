2343f974083d075936b1cbe2427f1527dc14ca5e
Mike Meyer <github.com@meyer.fm>
2016-11-23 20:26:45 -0800

    Use extracted CSSPropertyOperations module
    closes #50

--- src/createCSS.js
+++ src/createCSS.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var CSSPropertyOperations = require('react/lib/CSSPropertyOperations');
+var CSSPropertyOperations = require('react-css-property-operations');
 
 function createCSS(styles, className, comment, pseudoSelector) {
   if (!styles) {
