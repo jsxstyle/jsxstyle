e8194c5a286b0aae5b2ef28108eb2bdd73a15209
Mike Meyer <github.com@meyer.fm>
2017-04-26 21:38:11 -0700

    Update tests, tidy up a bit

--- packages/jsxstyle-loader/tests/example.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const {Block, InlineBlock} = require('../');
-
-<Block width="100%" height={25} left={2 * LayoutConstants.x} hoverColor="blue" hoverBackgroundColor="white">
-  <InlineBlock height={24} />
-  <div style={{width: 10}} />
-  <OtherComponent height={25} />
-</Block>;
--- packages/jsxstyle-loader/tests/extractStyles.spec.js
+++ packages/jsxstyle-loader/tests/extractStyles.spec.js
@@ -1,145 +1,250 @@
 'use strict';
 
 const extractStyles = require('../lib/ast-utils/extractStyles');
-const fs = require('fs');
-const path = require('path');
 
-const EXAMPLE_SRC = fs.readFileSync(path.join(__dirname, 'example.js'), {encoding: 'utf8'});
+const staticNamespace = {
+  LC: {
+    staticValue: 'ok',
+  },
+  val: 'thing',
+};
 
 describe('extractStyles', function() {
-  it('can extract constant styles', function() {
+  it('converts jsxstyle elements to plain elements when all props are static', () => {
     const rv = extractStyles({
-      src: EXAMPLE_SRC,
-      sourceFileName: 'test/constant-styles.js',
-      staticNamespace: {LayoutConstants: {x: 10}},
-      styleGroups: {
-        _test: {
-          display: 'block',
-          hoverColor: 'blue',
-        },
+      src: '<Block staticString="wow" staticInt={69} staticValue={val} staticMemberExpression={LC.staticValue} />',
+      sourceFileName: 'test/extract-static1.js',
+      cacheObject: {},
+      staticNamespace,
+    });
+
+    expect(rv.js).toEqual(
+      `require("test/extract-static1.jsxstyle.css");
+<div className="_x0" />`
+    );
+    expect(rv.css).toEqual(
+      `/* test/extract-static1.js:1 (Block) */
+._x0 {
+  static-string:wow;
+  static-int:69px;
+  static-value:thing;
+  static-member-expression:ok;
+  display:block;
+}
+`
+    );
+  });
+
+  it('converts jsxstyle elements to Block elements when some props aren\u2019t static', () => {
+    const rv = extractStyles({
+      src: '<Block staticString="wow" staticInt={69} staticValue={val} staticMemberExpression={LC.staticValue} dynamicValue={notStatic} />',
+      sourceFileName: 'test/extract-static2.js',
+      cacheObject: {},
+      staticNamespace,
+    });
+
+    expect(rv.js).toEqual(
+      `require("test/extract-static2.jsxstyle.css");
+<Block dynamicValue={notStatic} className="_x0" />`
+    );
+    expect(rv.css).toEqual(
+      `/* test/extract-static2.js:1 (Block) */
+._x0 {
+  static-string:wow;
+  static-int:69px;
+  static-value:thing;
+  static-member-expression:ok;
+}
+`
+    );
+  });
+
+  it('handles props mixed with spread operators', () => {
+    const rv = extractStyles({
+      src: `<Block doNotExtract {...spread} extract="yep" />`,
+      sourceFileName: 'test/spread.js',
+      cacheObject: {},
+    });
+
+    expect(rv.js).toEqual(
+      `require("test/spread.jsxstyle.css");
+<Block doNotExtract {...spread} extract={null} className="_x0" />`
+    );
+    expect(rv.css).toEqual(
+      `/* test/spread.js:1 (Block) */
+._x0 {
+  extract:yep;
+}
+`
+    );
+  });
+
+  it('updates `cacheObject` counter and key object', () => {
+    const cacheObject = {};
+
+    extractStyles({
+      src: `<Block />`,
+      sourceFileName: 'test/cache-object.js',
+      cacheObject,
+    });
+
+    extractStyles({
+      src: `<Block staticThing="wow" />`,
+      sourceFileName: 'test/cache-object.js',
+      cacheObject,
+    });
+
+    extractStyles({
+      src: `<InlineBlock />`,
+      sourceFileName: 'test/cache-object.js',
+      cacheObject,
+    });
+
+    expect(cacheObject).toEqual({
+      counter: 3,
+      keys: {
+        'display:block;': '0',
+        'display:block;staticThing:wow;': '1',
+        'display:inline-block;': '2',
       },
-      cacheObject: {},
     });
-    expect(rv.js).toEqual(
-      `require("test/constant-styles.jsxstyle.css");
-const React = require('react');
-const {Block, InlineBlock} = require('../');
+  });
 
-<div className="_test _x0">
-  <div className="_x1" />
-  <div style={{width: 10}} />
-  <OtherComponent height={25} />
-</div>;
-`
+  it('groups styles when a `styleGroups` object is provided', () => {
+    const cacheObject = {};
+    const styleGroups = {
+      _test1: {
+        thing: 'wow',
+        hoverThing: 'ok',
+      },
+      _test2: {
+        display: 'inline-block',
+      },
+    };
+
+    const rv = extractStyles({
+      src: `<Block>
+  <Block thing="wow" hoverThing="ok" />
+  <InlineBlock />
+</Block>`,
+      sourceFileName: 'test/style-groups.js',
+      cacheObject,
+      styleGroups,
+    });
+
+    expect(rv.js).toEqual(
+      `require("test/style-groups.jsxstyle.css");
+<div className="_x0">
+  <div className="_test1 _x0" />
+  <div className="_test2" />
+</div>`
     );
 
     expect(rv.css).toEqual(
-      `/* test/constant-styles.js:4 (Block) */
-._test {
+      `/* test/style-groups.js:1 (Block) */
+/* test/style-groups.js:2 (Block) */
+._x0 {
   display:block;
 }
-._test:hover {
-  color:blue;
+/* test/style-groups.js:2 (Block) */
+._test1 {
+  thing:wow;
 }
-/* test/constant-styles.js:4 (Block) */
-._x0 {
-  width:100%;
-  height:25px;
-  left:20px;
+._test1:hover {
+  thing:ok;
 }
-._x0:hover {
-  background-color:white;
-}
-/* test/constant-styles.js:5 (InlineBlock) */
-._x1 {
-  height:24px;
+/* test/style-groups.js:3 (InlineBlock) */
+._test2 {
   display:inline-block;
 }
 `
     );
   });
 
-  it('can extract simple expressions', function() {
-    const rv = extractStyles({
-      src: EXAMPLE_SRC,
-      sourceFileName: 'test/extract-expressions.js',
-      staticNamespace: {LayoutConstants: {x: 10}},
+  it('handles the `props` prop correctly', () => {
+    const rv1 = extractStyles({
+      src: `<Block props={{staticObject: 'yep'}} />;
+<Block props={{}} />;
+<Block props={variable} />;
+<Block props={calledFunction()} />;
+<Block props={member.expression} />;
+<Block dynamic={ok} props="this should remain untouched" />;`,
+      sourceFileName: 'test/props-prop1.js',
       cacheObject: {},
+      staticNamespace,
     });
 
-    expect(rv.js).toEqual(
-      `require("test/extract-expressions.jsxstyle.css");
-const React = require('react');
-const {Block, InlineBlock} = require('../');
-
-<div className="_x0">
-  <div className="_x1" />
-  <div style={{width: 10}} />
-  <OtherComponent height={25} />
-</div>;
-`
+    expect(rv1.js).toEqual(
+      `require("test/props-prop1.jsxstyle.css");
+<div staticObject="yep" className="_x0" />;
+<div className="_x0" />;
+<div {...variable} className="_x0" />;
+<div {...calledFunction()} className="_x0" />;
+<div {...member.expression} className="_x0" />;
+<Block dynamic={ok} props="this should remain untouched" />;`
     );
 
-    expect(rv.css).toEqual(
-      `/* test/extract-expressions.js:4 (Block) */
-._x0 {
-  width:100%;
-  height:25px;
-  left:20px;
-  display:block;
-}
-._x0:hover {
-  color:blue;
-  background-color:white;
-}
-/* test/extract-expressions.js:5 (InlineBlock) */
-._x1 {
-  height:24px;
-  display:inline-block;
-}
-`
-    );
+    expect(() =>
+      extractStyles({
+        src: '<Block props="invalid" />',
+        sourceFileName: 'test/props-prop2.js',
+        cacheObject: {},
+        staticNamespace,
+      })
+    ).toThrow(/`props` prop value was not handled by extractStyles: `"invalid"`/);
   });
 
-  it('can create nice looking css', function() {
+  it('handles the `component` prop correctly', () => {
     const rv = extractStyles({
-      src: EXAMPLE_SRC,
-      sourceFileName: 'test/nice-looking.js',
-      staticNamespace: {LayoutConstants: {x: 10}},
+      src: `<Block component="input" />;
+<Block component={Thing} />;
+<Block component={thing.cool} />;`,
+      sourceFileName: 'test/component-prop1.js',
       cacheObject: {},
+      staticNamespace,
     });
 
     expect(rv.js).toEqual(
-      `require("test/nice-looking.jsxstyle.css");
-const React = require('react');
-const {Block, InlineBlock} = require('../');
-
-<div className="_x0">
-  <div className="_x1" />
-  <div style={{width: 10}} />
-  <OtherComponent height={25} />
-</div>;
-`
+      `require("test/component-prop1.jsxstyle.css");
+<input className="_x0" />;
+<Thing className="_x0" />;
+<thing.cool className="_x0" />;`
     );
 
-    expect(rv.css).toEqual(
-      `/* test/nice-looking.js:4 (Block) */
-._x0 {
-  width:100%;
-  height:25px;
-  left:20px;
-  display:block;
-}
-._x0:hover {
-  color:blue;
-  background-color:white;
-}
-/* test/nice-looking.js:5 (InlineBlock) */
-._x1 {
-  height:24px;
-  display:inline-block;
-}
-`
-    );
+    expect(() =>
+      extractStyles({
+        src: '<Block component="CapitalisedString" />',
+        sourceFileName: 'test/component-prop2.js',
+        cacheObject: {},
+        staticNamespace,
+      })
+    ).toThrow(/`component` prop is a string that starts with an uppercase letter \(`CapitalisedString`\)/);
+
+    expect(() =>
+      extractStyles({
+        src: '<Block component={lowercaseIdentifier} />',
+        sourceFileName: 'test/component-prop3.js',
+        cacheObject: {},
+        staticNamespace,
+      })
+    ).toThrow(/`component` prop is an identifier that starts with a lowercase letter \(`lowercaseIdentifier`\)/);
+
+    expect(() =>
+      extractStyles({
+        src: '<Block component={functionCall()} />',
+        sourceFileName: 'test/component-prop4.js',
+        cacheObject: {},
+        staticNamespace,
+      })
+    ).toThrow(/`component` prop value was not handled by extractStyles: `functionCall\(\)`/);
+
+    expect(() =>
+      extractStyles({
+        src: '<Block component={member.expression()} />',
+        sourceFileName: 'test/component-prop4.js',
+        cacheObject: {},
+        staticNamespace,
+      })
+    ).toThrow(/`component` prop value was not handled by extractStyles: `member.expression\(\)`/);
   });
 });
