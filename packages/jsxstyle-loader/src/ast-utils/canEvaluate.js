3799862416af424cf7a7d955c7b7453a4a080b8f
Mike Meyer <github.com@meyer.fm>
2017-04-29 14:29:19 -0700

    Handle computed properties in canEvaluate

--- packages/jsxstyle-loader/src/ast-utils/canEvaluate.js
+++ packages/jsxstyle-loader/src/ast-utils/canEvaluate.js
@@ -7,10 +7,13 @@ const n = types.namedTypes;
 
 function canEvaluate(staticNamespace, exprNode) {
   if (n.Literal.check(exprNode)) {
+    // exprNode is a string, int, or null
     return true;
   } else if (n.JSXExpressionContainer.check(exprNode)) {
+    // exprNode is wrapped in an expression container
     return canEvaluate(staticNamespace, exprNode.expression);
   } else if (n.Identifier.check(exprNode)) {
+    // exprNode is a variable
     if (
       typeof staticNamespace === 'object' &&
       staticNamespace !== null &&
@@ -20,10 +23,20 @@ function canEvaluate(staticNamespace, exprNode) {
     }
     return false;
   } else if (n.MemberExpression.check(exprNode)) {
-    return n.Identifier.check(exprNode.property) && canEvaluate(staticNamespace, exprNode.object);
+    // exprNode is a member expression (object.property or object['property'])
+    return (
+      // object is in the provided namespace
+      canEvaluate(staticNamespace, exprNode.object) &&
+      // property is either an identifier specified with dot notation
+      ((n.Identifier.check(exprNode.property) && !exprNode.computed) ||
+        // or it's specified with bracket notation and can be evaluated
+        canEvaluate(staticNamespace, exprNode.property))
+    );
   } else if (n.BinaryExpression.check(exprNode)) {
+    // exprNode is a binary expression and both sides can be evaluated
     return canEvaluate(staticNamespace, exprNode.left) && canEvaluate(staticNamespace, exprNode.right);
   } else if (n.TemplateLiteral.check(exprNode)) {
+    // exprNode is a template literal and all expressions in the template literal can be evaluated
     for (let idx = -1, len = exprNode.expressions.length; ++idx < len; ) {
       const expr = exprNode.expressions[idx];
       if (!canEvaluate(staticNamespace, expr)) {
---
+++ packages/jsxstyle-loader/tests/canEvaluate.spec.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const canEvaluate = require('../src/ast-utils/canEvaluate');
+const parse = require('../src/ast-utils/parse');
+
+const staticNamespace = {
+  LC: {
+    thing1: 'wow',
+    thing2: 'ok',
+    thing3: 'cool',
+  },
+  nestedThing: {
+    level1: {
+      level2: 'level3',
+    },
+  },
+  staticValue: 69,
+  staticKey: 'thing3',
+};
+
+describe('canEvaluate', () => {
+  it('will not evaluate dynamic props', () => {
+    const ast = parse(
+      `<Block
+  dynamicValue={dynamicValue}
+  dynamicComputedProperty={LC[dynamicValue].thing}
+  templateString={\`thing\${one}\`}
+  complexExpression={thing ? 69 : 420}
+  binaryExpression={a + 'b'}
+/>`
+    );
+
+    const errors = [];
+    ast.program.body[0].expression.openingElement.attributes.forEach(attr => {
+      if (canEvaluate(staticNamespace, attr.value)) {
+        errors.push(`'${attr.name.name}' should be evaluated`);
+      }
+    });
+    expect(errors).toEqual([]);
+  });
+
+  it('will evaluate static props', () => {
+    const ast = parse(
+      `<Block
+  staticValue={staticValue}
+  memberExpression={LC.thing1}
+  computedProperty1={LC['thing2']}
+  computedProperty2={LC[staticKey]}
+  computedProperty3={LC['thing' + 1]}
+  computedProperty4={LC[\`thing\${2}\`]}
+  binaryExpression={staticValue + 420 + 'wow'}
+  kitchenSink={nestedThing.level1['level2'][staticValue]}
+/>`
+    );
+
+    const errors = [];
+    ast.program.body[0].expression.openingElement.attributes.forEach(attr => {
+      if (!canEvaluate(staticNamespace, attr.value)) {
+        errors.push(`'${attr.name.name}' should not be evaluated`);
+      }
+    });
+
+    expect(errors).toEqual([]);
+  });
+});
