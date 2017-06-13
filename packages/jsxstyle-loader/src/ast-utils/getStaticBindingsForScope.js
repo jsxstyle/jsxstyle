23eed39d31e0376b38747074a3a124b1cd929624
Mike Meyer <github.com@meyer.fm>
2017-06-12 19:00:32 -0700

    Only evaluate constant props
    this is a work in progress

--- packages/jsxstyle-loader/src/ast-utils/getStaticBindingsForScope.js
+++ packages/jsxstyle-loader/src/ast-utils/getStaticBindingsForScope.js
@@ -40,7 +40,10 @@ function getStaticBindingsForScope(scope, whitelist = [], sourceFileName) {
         ret[k] = require(moduleName);
       }
       continue;
-    } else if (t.isVariableDeclaration(binding.path.parent)) {
+    } else if (
+      t.isVariableDeclaration(binding.path.parent) &&
+      binding.path.parent.kind === 'const'
+    ) {
       // console.log(generate(binding.path.parent.node).code);
       // pick out the right variable declarator
       const dec = binding.path.parent.declarations.find(
