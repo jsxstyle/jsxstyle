04b09f5c26906c5c7e0258ceda7242a18f4625ce
Mike Meyer <github.com@meyer.fm>
2017-07-08 13:08:39 -0700

    Update example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,8 +2,11 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-  entry: require.resolve('./main'),
+module.exports = (env, options) => ({
+  entry: [
+    options.hot && 'react-hot-loader/patch',
+    require.resolve('./main'),
+  ].filter(f => f),
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js',
@@ -11,30 +14,54 @@ module.exports = {
   plugins: [
     new webpack.NamedModulesPlugin(),
     new HtmlWebpackPlugin({
-      template: 'template.html',
+      template: __dirname + '/template.html',
       inject: false,
     }),
-    new ExtractTextPlugin('bundle.css'),
-  ],
+    !options.hot && new ExtractTextPlugin('bundle.css'),
+  ].filter(f => f),
   resolve: {
     alias: {
       jsxstyle: require.resolve('../'),
     },
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: require.resolve('babel-loader'),
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [
+            [
+              'env',
+              {
+                targets: {
+                  browsers: ['last 2 versions'],
+                },
+              },
+            ],
+            'react',
+          ],
+          plugins: [
+            options.hot && 'react-hot-loader/babel',
+            'transform-object-rest-spread',
+            'transform-object-assign',
+          ].filter(f => f),
+        },
       },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
-      },
+      options.hot
+        ? {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          }
+        : {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: 'css-loader',
+            }),
+          },
     ],
   },
-};
+});
