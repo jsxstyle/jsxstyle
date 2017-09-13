c13b51d93370a92dd84eed55bdc3160c96f5fa85
Mike Meyer <github.com@meyer.fm>
2017-09-13 15:10:03 -0700

    Fix nondeterministic test

--- packages/jsxstyle-loader/tests/webpack.spec.js
+++ packages/jsxstyle-loader/tests/webpack.spec.js
@@ -6,46 +6,32 @@ const webpack = require('webpack');
 
 // TODO: evaluate webpack bundle
 it('builds without issue', function() {
-  const config = webpackConfig({multipleEntrypoints: true}, {});
+  const config = webpackConfig({ multipleEntrypoints: true });
   const compiler = webpack(config);
   const fs = new webpack.MemoryOutputFileSystem();
   compiler.outputFileSystem = fs;
 
-  expect.assertions(1);
-  const compilePromise = new Promise((resolve, reject) => {
+  expect.assertions(4);
+  return new Promise((resolve, reject) => {
     compiler.run(err => {
       if (err) reject(err);
-      resolve({
-        red: fs.readFileSync(
-          path.join(config.output.path, 'bundle-red.css'),
-          'utf8'
-        ),
-        blue: fs.readFileSync(
-          path.join(config.output.path, 'bundle-blue.css'),
-          'utf8'
-        ),
-      });
-    });
-  });
 
-  return expect(compilePromise).resolves.toEqual({
-    red: `/* ./tests/webpack/test-app/RedApp.js:8 (Block) */
-._x0 {
-  color: red;
-  display: block;
-  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-  font-size: 18px;
-  line-height: 22px;
-}
-`,
-    blue: `/* ./tests/webpack/test-app/BlueApp.js:8 (Block) */
-._x1 {
-  color: blue;
-  display: block;
-  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-  font-size: 18px;
-  line-height: 22px;
-}
-`,
+      const redCSS = fs.readFileSync(
+        path.join(config.output.path, 'bundle-red.css'),
+        'utf8'
+      );
+      const blueCSS = fs.readFileSync(
+        path.join(config.output.path, 'bundle-blue.css'),
+        'utf8'
+      );
+
+      expect(redCSS).toMatch(/Shared\.js:8 \(Block\)/);
+      expect(redCSS).toMatch(/color: red;/);
+
+      expect(blueCSS).toMatch(/Shared\.js:8 \(Block\)/);
+      expect(blueCSS).toMatch(/color: blue;/);
+
+      resolve();
+    });
   });
 });
