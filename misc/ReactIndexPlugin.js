bb0662d836a3172230a21014d428cc180a49e7f9
Mike Meyer <github.com@meyer.fm>
2018-04-16 00:08:17 -0700

    Fix ReactIndexPlugin

--- misc/ReactIndexPlugin.js
+++ misc/ReactIndexPlugin.js
@@ -2,43 +2,43 @@
 
 const path = require('path');
 
+function emitPlugin(compilation, callback) {
+  const statsObj = compilation.getStats().toJson();
+
+  for (const entryName in statsObj.assetsByChunkName) {
+    const bundleFile = path.join(
+      statsObj.publicPath,
+      Array.isArray(statsObj.assetsByChunkName[entryName])
+        ? statsObj.assetsByChunkName[entryName][0]
+        : statsObj.assetsByChunkName[entryName]
+    );
+
+    const indexFileContents = []
+      .concat(
+        '<!doctype html>',
+        '<title>jsxstyle demo</title>',
+        '<div id=".jsxstyle-demo"></div>',
+        `<script src="${bundleFile}"></script>`
+      )
+      .join('\n');
+
+    const fileName = (entryName === 'main' ? 'index' : entryName) + '.html';
+
+    compilation.assets[fileName] = {
+      source: () => indexFileContents,
+      size: () => indexFileContents.length,
+    };
+  }
+
+  callback();
+}
+
 class ReactIndexPlugin {
-  emitPlugin = (compilation, callback) => {
-    const statsObj = compilation.getStats().toJson();
-
-    for (const entryName in statsObj.assetsByChunkName) {
-      const bundleFile = path.join(
-        statsObj.publicPath,
-        Array.isArray(statsObj.assetsByChunkName[entryName])
-          ? statsObj.assetsByChunkName[entryName][0]
-          : statsObj.assetsByChunkName[entryName]
-      );
-
-      const indexFileContents = []
-        .concat(
-          '<!doctype html>',
-          '<title>jsxstyle demo</title>',
-          '<div id=".jsxstyle-demo"></div>',
-          `<script src="${bundleFile}"></script>`
-        )
-        .join('\n');
-
-      const fileName = (entryName === 'main' ? 'index' : entryName) + '.html';
-
-      compilation.assets[fileName] = {
-        source: () => indexFileContents,
-        size: () => indexFileContents.length,
-      };
-    }
-
-    callback();
-  };
-
   apply(compiler) {
     if (compiler.hooks) {
-      compiler.hooks.emit.tap('ReactIndexPlugin', this.emitPlugin);
+      compiler.hooks.emit.tap('ReactIndexPlugin', emitPlugin);
     } else {
-      compiler.plugin('emit', this.emitPlugin);
+      compiler.plugin('emit', emitPlugin);
     }
   }
 }
