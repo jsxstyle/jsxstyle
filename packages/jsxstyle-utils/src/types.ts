616ba463fc7830ffb61216845b1875037a55af98
Mike Meyer <github.com@meyer.fm>
2018-05-04 14:33:14 -0700

    Permit falsey style prop values in CSSProperties
    closes #114

--- packages/jsxstyle-utils/src/types.ts
+++ packages/jsxstyle-utils/src/types.ts
@@ -4,7 +4,19 @@ export interface Dict<T> {
   [key: string]: T;
 }
 
-export type ExactCSSProperties = Properties<string | number>;
+/** Make all properties in T potentially falsey */
+export type Falsey<T> = { [P in keyof T]?: T[P] | false | null };
 
-// Dict<any> is required until pseudoclass prefixed style props have types
+/**
+ * jsxstyle-compatible CSS properties provided by csstype.
+ *
+ * Use this type instead of `CSSProperties` if you don't use pseudoelement, pseudoclass, or media query props with jsxstyle.
+ */
+export type ExactCSSProperties = Falsey<Properties<string | number>>;
+
+/**
+ * jsxstyle-compatible CSS properties provided by csstype with an additional string index signature.
+ *
+ * Use this type instead of `ExactCSSProperties` if you use pseudoelement, pseudoclass, or media query props with jsxstyle.
+ */
 export type CSSProperties = ExactCSSProperties & Dict<any>;
