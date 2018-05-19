d155ed8a140faf63a2d512bd67f6a8c993266298
Mike Meyer <github.com@meyer.fm>
2018-05-18 17:27:57 -0700

    Convert CSSProperties from types to interfaces

--- packages/jsxstyle-utils/src/types.ts
+++ packages/jsxstyle-utils/src/types.ts
@@ -12,11 +12,12 @@ export type Falsey<T> = { [P in keyof T]?: T[P] | false | null };
  *
  * Use this type instead of `CSSProperties` if you don't use pseudoelement, pseudoclass, or media query props with jsxstyle.
  */
-export type ExactCSSProperties = Falsey<Properties<string | number>>;
+export interface ExactCSSProperties
+  extends Falsey<Properties<string | number>> {}
 
 /**
  * jsxstyle-compatible CSS properties provided by csstype with an additional string index signature.
  *
  * Use this type instead of `ExactCSSProperties` if you use pseudoelement, pseudoclass, or media query props with jsxstyle.
  */
-export type CSSProperties = ExactCSSProperties & Dict<any>;
+export interface CSSProperties extends ExactCSSProperties, Dict<any> {}
