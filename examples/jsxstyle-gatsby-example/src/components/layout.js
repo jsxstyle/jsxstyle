0e42fe6f6396fb16d61031aa5e383b6059abf19a
Mike Meyer <github.com@meyer.fm>
2018-06-23 12:56:01 -0700

    Remove file: URLs from examples
    using a `packages` → `examples/node_modules` symlink instead

--- examples/jsxstyle-gatsby-example/src/components/layout.js
+++ examples/jsxstyle-gatsby-example/src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { StaticQuery } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import { Block } from 'jsxstyle';
 
 import Header from './header';
