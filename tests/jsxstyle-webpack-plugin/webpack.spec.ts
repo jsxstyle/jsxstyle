e6c89b5e7e0ad8294a91bfb003ef6a9018fd92a3
meyer <github.com@meyer.fm>
2018-08-06 15:46:38 -0700

    Fix nondeterministic webpack test

--- tests/jsxstyle-webpack-plugin/webpack.spec.ts
+++ tests/jsxstyle-webpack-plugin/webpack.spec.ts
@@ -12,7 +12,7 @@ it('builds without issue', () => {
   const fs = new MemoryFS();
   compiler.outputFileSystem = fs;
 
-  expect.assertions(2);
+  expect.assertions(4);
   return new Promise((resolve, reject) => {
     compiler.run((err: Error & { details: any }, stats) => {
       if (err) {
@@ -40,8 +40,38 @@ it('builds without issue', () => {
         'utf8'
       );
 
-      expect(redCSS).toMatchSnapshot();
-      expect(blueCSS).toMatchSnapshot();
+      const sharedStyles = `
+/* ./jsxstyle-webpack-plugin/webpack/test-app/Shared.js:8 (Block) */
+._1qb53c2 {
+  display: block;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+  font-size: 18px;
+  line-height: 22px;
+}
+      `.trim();
+
+      expect(redCSS).toContain(
+        `
+/* ./jsxstyle-webpack-plugin/webpack/test-app/RedApp.js:8 (Inline) */
+._1ioutjs {
+  color: red;
+  display: inline;
+}
+`.trim()
+      );
+
+      expect(blueCSS).toContain(
+        `
+/* ./jsxstyle-webpack-plugin/webpack/test-app/BlueApp.js:8 (Inline) */
+._1qr3dx1 {
+  color: blue;
+  display: inline;
+}
+`.trim()
+      );
+
+      expect(redCSS).toContain(sharedStyles);
+      expect(blueCSS).toContain(sharedStyles);
 
       resolve();
     });
