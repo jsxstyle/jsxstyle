d8f75471a5d3622fc758c6c501db7ec704bda959
Andrey Morozov <andreym@hzdg.com>
2018-10-29 10:52:34 -0400

    Providing cached styles to webpack loader

--- packages/gatsby-plugin-jsxstyle/gatsby-node.js
+++ packages/gatsby-plugin-jsxstyle/gatsby-node.js
@@ -1,18 +1,21 @@
 const JsxstylePlugin = require('jsxstyle-webpack-plugin');
+const path = require('path');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ actions, cache }) => {
   actions.setWebpackConfig({
     plugins: [new JsxstylePlugin()],
     module: {
       rules: [
         {
           test: /\.(?:jsx?|tsx)$/,
-          use: [{
-            loader: JsxstylePlugin.loader,
-            options: {
-              classNameFormat: 'hash'
+          use: [
+            {
+              loader: JsxstylePlugin.loader,
+              options: {
+                cacheFile: path.resolve(cache.directory, 'style-key-cache.txt'),
+              },
             },
-          }],
+          ],
         },
       ],
     },
