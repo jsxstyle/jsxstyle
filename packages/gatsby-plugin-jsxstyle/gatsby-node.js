f8b921da6630308386ef2d96aa4bb70fcea5a79a
Andrey Morozov <andreym@hzdg.com>
2018-10-31 18:48:27 -0400

    Enable cacheFile option in gatsby-plugin-jsxstyle
    closes #122

--- packages/gatsby-plugin-jsxstyle/gatsby-node.js
+++ packages/gatsby-plugin-jsxstyle/gatsby-node.js
@@ -1,13 +1,21 @@
 const JsxstylePlugin = require('jsxstyle-webpack-plugin');
+const path = require('path');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ actions, cache }) => {
   actions.setWebpackConfig({
     plugins: [new JsxstylePlugin()],
     module: {
       rules: [
         {
           test: /\.(?:jsx?|tsx)$/,
-          use: [JsxstylePlugin.loader],
+          use: [
+            {
+              loader: JsxstylePlugin.loader,
+              options: {
+                cacheFile: path.resolve(cache.directory, 'style-key-cache.txt'),
+              },
+            },
+          ],
         },
       ],
     },
