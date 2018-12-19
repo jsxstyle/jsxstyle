eee7c9e321d106fdc964f57bb37a6310df58b3f9
chenxsan <chenxsan@gmail.com>
2018-12-19 14:49:17 +0800

    Allow array-type parserPlugins
    closes #127

--- packages/jsxstyle-webpack-plugin/src/utils/ast/parse.ts
+++ packages/jsxstyle-webpack-plugin/src/utils/ast/parse.ts
@@ -5,19 +5,17 @@ export function parse(
   plugins: babelParser.ParserPlugin[] = []
 ): any {
   return babelParser.parse(code.toString(), {
-    plugins: Array.from(
-      new Set<babelParser.ParserPlugin>([
-        'asyncGenerators',
-        'classProperties',
-        'dynamicImport',
-        'functionBind',
-        'jsx',
-        'numericSeparator',
-        'objectRestSpread',
-        'optionalCatchBinding',
-        ...plugins,
-      ])
-    ),
+    plugins: [
+      'asyncGenerators',
+      'classProperties',
+      'dynamicImport',
+      'functionBind',
+      'jsx',
+      'numericSeparator',
+      'objectRestSpread',
+      'optionalCatchBinding',
+      ...plugins,
+    ],
     sourceType: 'module',
   });
 }
