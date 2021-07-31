86f9eb7b40247656a5d0de0e12013f1d033884ae
meyer <github.com@meyer.fm>
2021-07-31 15:07:53 -0400

    Fix webpack 5 warning in ReactIndexPlugin

--- examples/ReactIndexPlugin.js
+++ examples/ReactIndexPlugin.js
@@ -1,40 +1,43 @@
 'use strict';
 
-function emitPlugin(compilation, callback) {
-  const indexFileContents = `<!doctype html>
+const indexFileContents = `<!doctype html>
 <title>jsxstyle demo</title>
 <div id=".jsxstyle-demo"></div>
 <script src="/bundle.js"></script>
 `;
 
-  const fileName = 'index.html';
+const fileName = 'index.html';
 
-  const source = {
-    source: () => indexFileContents,
-    size: () => indexFileContents.length,
-  };
+const source = {
+  source: () => indexFileContents,
+  size: () => indexFileContents.length,
+};
 
-  if (compilation.emitAsset) {
-    compilation.emitAsset(fileName, source);
-  } else {
-    compilation.assets[fileName] = source;
-  }
+const emitPlugin = (compilation, callback) => {
+  compilation.assets[fileName] = source;
 
   if (callback) {
     callback();
   }
-}
+};
 
 class ReactIndexPlugin {
-  apply(compiler) {
+  apply(
+    /** @type {import('webpack').Compiler} */
+    compiler
+  ) {
     if (compiler.hooks) {
-      if (compiler.hooks.processAssets) {
-        // webpack 5
-        compiler.hooks.processAssets.tap('ReactIndexPlugin', emitPlugin);
-      } else {
-        // webpack 4
-        compiler.hooks.emit.tap('ReactIndexPlugin', emitPlugin);
-      }
+      compiler.hooks.make.tap('ReactIndexPlugin', (compilation) => {
+        if ('processAssets' in compilation.hooks) {
+          // webpack 5
+          compilation.hooks.processAssets.tap('ReactIndexPlugin', () => {
+            compilation.emitAsset(fileName, source);
+          });
+        } else {
+          // webpack 4
+          compiler.hooks.emit.tap('ReactIndexPlugin', emitPlugin);
+        }
+      });
     } else {
       // webpack 1-3
       compiler.plugin('emit', emitPlugin);
