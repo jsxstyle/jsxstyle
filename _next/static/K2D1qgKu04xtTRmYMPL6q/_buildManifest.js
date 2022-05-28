19b1b84fff587f37d1643019929d1b5b3caa8631
meyer <github.com@meyer.fm>
2022-05-27 22:51:44 -0700

    Manual update

---
+++ _next/static/K2D1qgKu04xtTRmYMPL6q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-263207b652d86029.js"],"/_error":["static/chunks/pages/_error-b7479729f8eba6b5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
