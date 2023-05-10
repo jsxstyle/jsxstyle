782145b961982a1258157db4231088b64ad9189e
meyer <github.com@meyer.fm>
2023-05-10 15:08:52 -0400

    Add `cacheFile` functionality to vite plugin

--- packages/jsxstyle-vite-plugin/src/index.ts
+++ packages/jsxstyle-vite-plugin/src/index.ts
@@ -11,32 +11,78 @@ interface PluginOptions
   extends UserConfigurableOptions,
     Pick<ExtractStylesOptions, 'modulesByAbsolutePath'> {
   extensions?: string[];
+  cacheFile?: string;
 }
 
 export const jsxstyleVitePlugin = ({
   extensions = ['.ts', '.tsx', '.js'],
   modulesByAbsolutePath,
+  cacheFile,
   ...options
 }: PluginOptions): Plugin => {
   const isHandledFile = (id: string) =>
     extensions.some((ext) => id.endsWith(ext));
 
   const cssContent: Record<string, string> = {};
+  const classNameCache: Record<string, string> = {};
 
   const getClassNameForKey = (() => {
-    const cache: Record<string, string> = {};
     let num = 0;
 
     return (content: string) => {
-      if (!cache[content]) {
-        cache[content] = `_x${num++}`;
+      if (!classNameCache[content]) {
+        classNameCache[content] = `_x${(num++).toString(36)}`;
       }
-      return cache[content];
+      return classNameCache[content];
     };
   })();
 
+  let buildStart: Plugin['buildStart'];
+  let buildEnd: Plugin['buildEnd'];
+
+  if (typeof cacheFile === 'string') {
+    buildStart = async () => {
+      try {
+        const cacheFileContents = await fs.readFile(cacheFile, {
+          encoding: 'utf8',
+          flag: 'r',
+        });
+
+        // create mapping of unique CSS strings to class names
+        const lines = new Set<string>(cacheFileContents.trim().split('\n'));
+        lines.forEach((line) => {
+          const trimmedLine = line.trim();
+          if (trimmedLine) {
+            // add each line of CSS to the cache
+            getClassNameForKey(trimmedLine);
+          }
+        });
+      } catch (error) {
+        if (error.code === 'EISDIR') {
+          throw new Error('Value of cacheFile is a directory');
+        } else if (error.code === 'ENOENT') {
+          console.log('Cache file does not exist and will be created');
+        } else {
+          throw error;
+        }
+      }
+    };
+
+    buildEnd = async () => {
+      try {
+        const cacheString =
+          Object.keys(classNameCache).filter(Boolean).join('\n') + '\n';
+        await fs.writeFile(cacheFile, cacheString, 'utf8');
+      } catch (error) {
+        console.error('Could not write cache file to `%s`:', cacheFile, error);
+      }
+    };
+  }
+
   return {
     name: 'jsxstyle-vite-plugin',
+    buildStart,
+    buildEnd,
     resolveId(id, importer) {
       if (!importer || !id.endsWith('__jsxstyle.css')) return;
       const importerDirName = path.dirname(importer);
