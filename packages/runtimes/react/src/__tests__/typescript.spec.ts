c688abbba7ff9337af2a40070bd108edae8f07da
meyer <github.com@meyer.fm>
2026-02-02 04:23:34 -0500

    Tidy up, use node 24 everywhere

--- packages/runtimes/react/src/__tests__/typescript.spec.ts
+++ packages/runtimes/react/src/__tests__/typescript.spec.ts
@@ -4,7 +4,7 @@ import { promisify } from 'node:util';
 const execAsync = promisify(exec);
 
 it('throws type errors for invalid component/prop types', async () => {
-  const report = await execAsync('npx tsc', { cwd: __dirname });
+  const report = await execAsync('pnpm exec tsc', { cwd: __dirname });
   expect(report.stderr).toEqual('');
   expect(report.stdout).toEqual('');
 });
